2cff421b8aed44f1657a50c9147e02db322a5496
amosperez <amos7perez@gmail.com>
2023-10-08 01:44:44 -0400

    Drag and Drop File Uploader

--- frontend/components/DragAndDropFileUploader.tsx
+++ frontend/components/DragAndDropFileUploader.tsx
@@ -11,6 +11,19 @@ export default function DragAndDropFileUploader() {
       setFile(file);
    };
    return (
-      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+      <FileUploader handleChange={handleChange} name="file" types={fileTypes}>
+         <div className="max-w-md">
+            <div>Upload</div>
+            <div className="mt-2 rounded-lg border border-dashed px-6 py-10 text-center hover:cursor-pointer">
+               <div className="font-bold">Drag and drop files here</div>
+
+               <div className="mt-2 text-xs text-gray-400">
+                  -------- or --------
+               </div>
+
+               <div className="mt-2">Browse Files</div>
+            </div>
+         </div>
+      </FileUploader>
    );
 }
